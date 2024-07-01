1ba7de7682d1b8dcb419e40eb855a8e63124804a
Jonas Bueno <jonas.bueno@opt.com.br>
2024-07-01 19:55:49 -0300

    :tada: Initial commit: recreating my portfolio from scratch.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h2>Hello. My name is Jonas.</h2>
+      <h1>And this is my Portfolio.</h1>
+    </>
+  )
+}
+
+export default App
