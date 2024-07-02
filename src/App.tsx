81a4d91d636123abd3f39842f7dea394b3e24916
Jonas Bueno <jonas.bueno@opt.com.br>
2024-07-02 11:53:59 -0300

    :bug: reconfiguring Tailwind for Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 
 import './App.css'
+import './index.css'
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <>
 
-      <h2>Hello. My name is Jonas.</h2>
-      <h1>And this is my Portfolio.</h1>
+
+    <div className="bg-base h-screen">
+
+      <Header></Header>
+
+      <h1 className="pt-4 text-primary">Hello. My name is Jonas.</h1>
+      <h1 className='text-primary'>And this is my Portfolio.</h1>
+
+    </div>
+
+
     </>
   )
 }
